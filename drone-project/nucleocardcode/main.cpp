d01d66d2f82b88bea13170ed40eb67b885f6c24f
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-02-14 22:53:29 +0100

    update

---
+++ drone-project/nucleocardcode/main.cpp
@@ -0,0 +1,14 @@
+#include "mbed.h"
+#include "errorcode.h"
+
+int main()
+{
+    while(1) {
+        buttonisused();
+        /*test code only to check if that working*/
+        errorcode(5, 3, 2);
+        valueofbuttonincrement();
+
+    }
+
+};s
